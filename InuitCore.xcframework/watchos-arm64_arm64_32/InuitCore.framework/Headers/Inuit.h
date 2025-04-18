382089f1004b2daa3dd8613ef8728a1751d50be9
Smukii <karlo.horvat@inova.si>
2025-04-18 12:53:15 +0200

    xcframework

---
+++ InuitCore.xcframework/watchos-arm64_arm64_32/InuitCore.framework/Headers/Inuit.h
@@ -0,0 +1,19 @@
+//
+//  Inuit.h
+//  Inuit
+//
+//  Created by Sašo Sečnjak on 31/08/16.
+//  Copyright © 2016 Inova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Inuit.
+FOUNDATION_EXPORT double InuitVersionNumber;
+
+//! Project version string for Inuit.
+FOUNDATION_EXPORT const unsigned char InuitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Inuit/PublicHeader.h>
+
+
