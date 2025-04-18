f86df02c9093213c40e3fc9a3780bfe3144f912d
Smukii <karlo.horvat@inova.si>
2025-04-18 13:15:13 +0200

    dsfa

--- Package.swift
+++ Package.swift
@@ -1,19 +1,14 @@
 import PackageDescription
 
 let package = Package(
-    name: "InuitCore",
-    platforms: [
-        .iOS(.v13),
-        .macOS(.v11)
-    ],
+    name: "Inuit Core",
     products: [
-        .library(name: "InuitCore", targets: ["InuitCore"])
+        .library(
+            name: "Inuit Core",
+            targets: ["InuitCore"])
     ],
+    dependencies: [],
     targets: [
-        .binaryTarget(
-            name: "InuitCore",
-            url: "https://github.com/YOU/InuitCoreBinary/releases/download/1.0.0/InuitCore.xcframework.zip",
-            checksum: "27d00ea43b42cd14a8c1f5ced10a2c58dd763eed854929578de565c3e8f65f7c"
-        )
+        .binaryTarget(name: "InuitCore", path: "./Sources/InuitCore.xcframework")
     ]
 )
---
+++ README.md
@@ -0,0 +1,11 @@
+# Inuit-Core-iOS
+Compiled InuitCore library for iOS. Created package (with Xcode 13.0) to use it with Swift Package Manager.
+
+## Build
+Build server: [http://makeit/job/Inuit-Swift-iOS/](http://makeit/job/Inuit-Swift-iOS/)
+
+## Xcode SDK
+Check build server job to see the Xcode version with which library was build with.
+
+## Copyright
+Inova IT d.o.o.
